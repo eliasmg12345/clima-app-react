07c92d7add71eaee0f5b22c106261f09e593b3bf
eliasmg12345 <randyofgod666@gmail.com>
2023-03-02 16:02:57 -0400

    primer

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+import useClima from "../hooks/useClima"
+
+
+const Formulario = () => {
+
+    const [alerta, setAlerta] = useState('')
+    const { busqueda, datosBusqueda, consultarClima } = useClima()
+    const { ciudad, pais } = busqueda
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (Object.values(busqueda).includes('')) {
+            setAlerta('Todos los campos son obligatorios')
+            return
+        }
+        setAlerta('')
+
+        consultarClima(busqueda)
+    }
+
+    
+    return (
+        <div className="contenedor">
+
+            {alerta && <p>{alerta}</p>}
+            <form action=""
+                onSubmit={handleSubmit}
+            >
+                <div className="campo">
+                    <label htmlFor="ciudad">Ciudad</label>
+                    <input
+                        type="text"
+                        id="ciudad"
+                        name="ciudad"
+                        onChange={datosBusqueda}
+                        value={ciudad}
+                    />
+                </div>
+
+                <div className="campo">
+                    <label htmlFor="pais">Pais</label>
+                    <select
+                        name="pais"
+                        id="pais"
+                        onChange={datosBusqueda}
+                        value={pais}
+                    >
+                        <option value="">Seleccione un pais</option>
+                        <option value="US">EEUU</option>
+                        <option value="BO ">Bolivia</option>
+                    </select>
+                </div>
+
+                <input
+                    type="submit"
+                    value="Consultar Clima"
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Formulario
